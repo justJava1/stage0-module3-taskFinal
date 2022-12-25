3db1aaf7f79741a9382c82b2fd83936cd1c845d5
Kirill Gusakov <justmymail003@gmail.com>
2022-12-25 17:16:54 +0300

    z

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(number);
+        sb.reverse();
+        System.out.println(sb);
     }
 }
